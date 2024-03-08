e18b94a7c98dd319f38f3ef9ab158c9f95e23c42
Isaksen <jan.isaksen@nordicsemi.no>
2024-03-08 15:06:37 +0100

    aaaaa

--- sw_src/Encoder_sim/ROC.c
+++ sw_src/Encoder_sim/ROC.c
@@ -6,27 +6,49 @@
 #include <stdbool.h>
 
 // MNIST images have been resized to 16x16
-#define IMAGE_SIZE 10
+#define IMAGE_SIZE 5
 
 // Each pixel on MNIST is a value from 0 to 255, stored as integers
-#define PIXEL_MAX_VALUE 20
+#define PIXEL_MAX_VALUE 10
 
 // Number of digits in the base 10 number system
 #define BASE_10 10
 
 
-void print_array(uint32_t array[]){
+void print_array(int array[]){
   printf("[");
-  for (uint32_t i = 0; i < IMAGE_SIZE; i++) {
-    printf("%" PRIu32, array[i]);
+  for (int i = 0; i < IMAGE_SIZE; i++) {
+    printf("%d", array[i]);
     if (i < IMAGE_SIZE - 1) {
-      printf(", "); 
+      printf(", \t"); 
     }
   }
   printf("]\n\n");
 }
 
-void sorter(uint32_t input_array[], uint32_t sorted_array[]){
+void print_indices(){
+  printf("[");
+  for (int i = 0; i < IMAGE_SIZE; i++) {
+    printf("%d", i);
+    if (i < IMAGE_SIZE - 1) {
+      printf(", \t"); 
+    }
+  }
+  printf("]\n");
+}
+
+void print_array_reverse(int array[]){
+  printf("[");
+  for (int i = IMAGE_SIZE - 1; i >= 0; i--) {
+    printf("%d", array[i]);
+    if (i > 0) {
+      printf(", \t"); 
+    }
+  }
+  printf("]\n\n");
+}
+
+void sorter(int input_array[], int sorted_array[]){
 
   /*  Encoding algorithm: O(n^2) = O(IMAGE_SIZE * PIXEL_MAX_VALUE)
       Works by iterating through each of the possible intensity values and 
@@ -35,10 +57,10 @@ void sorter(uint32_t input_array[], uint32_t sorted_array[]){
       store the value and increas the pointer to the next sotring location. 
       Stop when the array has been sorted. 
   */ 
-  uint32_t sorted_index = 0;
-  for (int32_t intensity = PIXEL_MAX_VALUE; intensity >= 0; intensity--) {
-    for (uint32_t pixelID = 0; pixelID < IMAGE_SIZE; pixelID++) {
-      if (input_array[pixelID] == (uint32_t)intensity){            
+  int sorted_index = 0;
+  for (int intensity = PIXEL_MAX_VALUE; intensity >= 0; intensity--) {
+    for (int pixelID = 0; pixelID < IMAGE_SIZE; pixelID++) {
+      if (input_array[pixelID] == intensity){            
         // Store the value to the sorted array
         sorted_array[sorted_index] = pixelID;
         sorted_index++;
@@ -50,77 +72,54 @@ void sorter(uint32_t input_array[], uint32_t sorted_array[]){
   }
 }
 
-void counting_sort(uint32_t input_array[], uint32_t sorted_array[]) {
+void counting_sort(int input_array[], int sorted_array[]) {
   // Array to store frequency of each intensity value
-  uint32_t frequency[PIXEL_MAX_VALUE+1] = {0};
+  int frequency[PIXEL_MAX_VALUE+1] = {0};
 
   // Calculate frequency of each intensity value
-  for (uint32_t pixelID = 0; pixelID < IMAGE_SIZE; pixelID++) {
+  for (int pixelID = 0; pixelID < IMAGE_SIZE; pixelID++) {
     frequency[input_array[pixelID]]++;
   }
 
   // Calculate cumulative sum of frequencies
-  for (int32_t intensity = PIXEL_MAX_VALUE - 1; intensity >= 0; intensity--) {
-    frequency[intensity] += frequency[intensity + 1];
+  for (int intensity = 1; intensity <= PIXEL_MAX_VALUE; intensity++) {
+    frequency[intensity] += frequency[intensity - 1];
   }
 
   // Fill sorted array using counting sort
-  for (uint32_t pixelID = 0; pixelID < IMAGE_SIZE; pixelID++) {
-    uint32_t intensity = input_array[pixelID];
+  for (int pixelID = 0; pixelID < IMAGE_SIZE; pixelID++) {
+    int intensity = input_array[pixelID];
     sorted_array[frequency[intensity] - 1] = pixelID;
     frequency[intensity]--;
   }
 }
 
-void counting_sort_radix(uint32_t input_array[], int exp, uint32_t sorted_array[]) {
-  int count[BASE_10] = {0}; // Array to store count of occurrences of each digit
-
-  // Count occurrences of digits
-  for (int i = 0; i < IMAGE_SIZE; i++)
-    count[(input_array[i] / exp) % BASE_10]++;
-
-  // Adjust count array
-  for (int i = BASE_10 - 2; i >= 0; i--)
-    count[i] += count[i + 1];
-
-  // Build the sorted array
-  for (int i = 0; i < IMAGE_SIZE; i++) {
-    sorted_array[count[(input_array[i] / exp) % BASE_10] - 1] = i;
-    count[(input_array[i] / exp) % BASE_10]--;
-  }
-}
-
-void radix_sort(uint32_t input_array[], uint32_t sorted_array[]) {
-  // Do counting sort for every digit
-  for (int exp = 1; PIXEL_MAX_VALUE / exp > 0; exp *= 10) {
-    counting_sort_radix(input_array, exp, sorted_array);
-  }
-}
 
 int main() {
-  uint32_t image[IMAGE_SIZE];
-  uint32_t sorted_indexes[IMAGE_SIZE];
+  int image[IMAGE_SIZE];
+  int sorted_indexes[IMAGE_SIZE];
 
   /*  Initialize each value randomly between 0 and 255. Seed the random number 
       generator to get different values each time
   */
+  printf("Indices: \t\t");
+  print_indices();
   srand(time(NULL));
-  for (uint32_t i = 0; i < IMAGE_SIZE; i++) {
-    image[i] = (uint32_t)rand() % PIXEL_MAX_VALUE + 1;
+  for (int i = 0; i < IMAGE_SIZE; i++) {
+    image[i] = rand() % PIXEL_MAX_VALUE + 1;
   }
+  printf("Original image: \t");
   print_array(image);
 
   /*  Start sorting the image - first solution */
   sorter(image, sorted_indexes);
+  printf("Sorted indices: \t");
   print_array(sorted_indexes); 
 
-  /*  Start sorting the image - counting sort not stable*/
+  /*  Start sorting the image - counting sort*/
   counting_sort(image, sorted_indexes);
-  print_array(sorted_indexes);
-
-   /*  Start sorting the image - radix sort*/
-  radix_sort(image, sorted_indexes);
-  print_array(sorted_indexes); 
+  printf("Counting sort: \t\t");
+  print_array_reverse(sorted_indexes);
 
   return 0;
 }
